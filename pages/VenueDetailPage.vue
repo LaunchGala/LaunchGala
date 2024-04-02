a9e3f965beaea92f27b7c743c144523aa2c53126
Rashid Refai <rifairashid@gmail.com>
2024-04-01 18:58:38 -0700

    VenueDetailPage fixing

--- pages/VenueDetailPage.vue
+++ pages/VenueDetailPage.vue
@@ -16,10 +16,17 @@
 //   PopoverTrigger
 // } from '@/components/ui/popover'
 // import { Calendar } from '@/components/ui/calendar'
-import { MapPin, Star, User, ArrowRight, Home, Check, Share2, Bookmark, Save } from 'lucide-vue-next'
+import { MapPin, Star, User, ArrowRight, Home, Check, Share2, Bookmark, Save, Clock, Info, Shield, XCircle } from 'lucide-vue-next'
 import { ref, defineProps } from 'vue'
 import { format } from 'date-fns'
 import { Button } from '@/components/ui/button'
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '@/components/ui/accordion';
 
 
 const date = ref<Date>()
@@ -73,11 +80,15 @@ const date = ref<Date>()
         
         <div class="flex items-center space-x-2">
           <Star class="w-5 h-5 text-yellow-400" />
-          <span class="text-lg font-semibold">4.98 (421 reviews)</span>
+          <span class="text-lg font-semibold">421 Endorsement</span>
         </div>
         <div class="flex items-center space-x-2">
           <User class="w-5 h-5" />
           <span>Hosted by <strong>Kevin</strong></span>
+        </div>
+        <div class="flex items-center space-x-2">
+          <User class="w-5 h-5" />
+          <span>Company <strong>Google</strong></span>
         </div>
         <div class="flex items-center space-x-2">
           <User class="w-5 h-5" />
@@ -87,11 +98,120 @@ const date = ref<Date>()
           <User class="w-5 h-5" />
           <span>Sponsor option: <strong>Yes</strong></span>
         </div>
+        <div class="flex items-center space-x-2">
+          <User class="w-5 h-5" />
+          <span>Venue type: <strong>Office room</strong></span>
+        </div>
+        <div class="flex items-center space-x-2">
+          <User class="w-5 h-5" />
+          <span>Total capacity/Venue size: <strong>180</strong></span>
+        </div>
+        <div class="flex items-center space-x-2">
+          <User class="w-5 h-5" />
+          <span>Multiple rooms: <strong>Yes</strong></span>
+        </div>
+        <div class="flex items-center space-x-2">
+          <User class="w-5 h-5" />
+          <span>Additional Insurance required: <strong>No</strong></span>
+        </div>
+        <div class="flex items-center space-x-2">
+          <User class="w-5 h-5" />
+          <span>Cleaning Fees: <strong>No</strong></span>
+        </div>
 
         
         <div class="flex items-center space-x-2">
           <MapPin class="w-5 h-5" />
           <span>San Francisco, United States</span>
+        </div>
+        <div>
+          <h3 class="font-semibold text-lg mb-2">Venue good for:</h3>
+          <div class="flex flex-wrap gap-2">
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Conferences
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Seminars and Workshops
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Trade Shows and Expos
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Business Meetings
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Networking Events
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Team Building & Training
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Product Launches
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Board Meetings
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Investor Relations Meetings
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Press Conferences
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Pitch Events
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Hackathons
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Symposiums
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Panel Discussions
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Round Table Discussions
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Lectures and Public Speaking Events
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Dinner Party
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Corporate Anniversaries
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Job Fairs and Career Expos
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Signings and Publishing Events
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Gala Event
+            </span>
+          </div>
         </div>
         <div>
           <h3 class="font-semibold text-lg mb-2">Amenities</h3>
@@ -106,22 +226,47 @@ const date = ref<Date>()
             </span>
             <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
               <Check class="w-4 h-4 text-green-500" />
-              Washer
+              Outdoor Area
             </span>
             <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
               <Check class="w-4 h-4 text-green-500" />
-              Iron
+              Kitchen
             </span>
             <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
               <Check class="w-4 h-4 text-green-500" />
-              TV
+              Bar Area
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Restrooms
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Parking
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Presentation Equipment
             </span>
             <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
               <Check class="w-4 h-4 text-green-500" />
               Private entrance
             </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Accessibility features
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Seating Furniture
+            </span>
+            <span class="flex items-center gap-1 bg-gray-200 dark:bg-gray-700 py-1 px-2 rounded">
+              <Check class="w-4 h-4 text-green-500" />
+              Audiovisual Equipment
+            </span>
           </div>
         </div>
+        <h3 class="font-semibold text-lg mb-2">Description:</h3>
         <p class="leading-relaxed">
           Discover the charms of the city in this exclusive apartment with fine finishes, thoughtful design, and a peaceful terrace views. After exploring the city, relax in the serene bedroom or enjoy the local restaurants within walking distance.
         </p>
@@ -137,23 +282,75 @@ const date = ref<Date>()
           </Button>
         </div>
         <div class="flex flex-col items-end space-y-2">
-          <Button class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 dark:bg-indigo-500 dark:hover:bg-indigo-600">
-            <span>Book now</span>
+          <Button class="inline-flex items-center px-4 py-2 text-sm font-semibold text-white OrangeCol hover:bg-gray-400 dark:hover:bg-gray-400">
+            <span>Contact Host</span>
             <ArrowRight class="w-4 h-4 ml-2" />
           </Button>
         </div>
       </CardFooter>
       <div class="px-4 py-2 dark:bg-gray-800">
-        <AspectRatio :ratio="16 / 9" class="rounded-lg overflow-hidden shadow-sm">
+        <AspectRatio class="rounded-lg overflow-hidden shadow-sm">
           <iframe 
             src="https://maps.google.com/maps?q=San%20Francisco,%20USA&t=&z=13&ie=UTF8&iwloc=&output=embed" 
             frameborder="0" 
-            class="w-full h-full" 
+            class="w-full min-h-screen rounded-md" 
             allowfullscreen 
             aria-hidden="false" 
             tabindex="0">
           </iframe>
         </AspectRatio>
+        
+        <div class="bg-white dark:bg-black p-6 shadow-lg rounded-lg  mx-auto my-8">
+    <h2 class="text-2xl font-semibold mb-4">Reservation Rules</h2>
+
+    <Accordion type="single" collapsible class="space-y-2">
+      <AccordionItem value="operating-hours">
+        <AccordionTrigger class="flex items-center justify-between">
+          <span class="font-medium">Operating Hours</span>
+          <!-- <Clock class="w-5 h-5 text-gray-500 dark:text-gray-400" /> -->
+        </AccordionTrigger>
+        <AccordionContent>
+          <p class="mt-2">Mon-Fri: 9am to 10pm</p>
+          <p>Sat-Sun: 10am to 11pm</p>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="safety-rules">
+        <AccordionTrigger class="flex items-center justify-between">
+          <span class="font-medium">Safety Rules</span>
+          <!-- <Shield class="w-5 h-5 text-gray-500 dark:text-gray-400" /> -->
+        </AccordionTrigger>
+        <AccordionContent>
+          <ul class="list-disc ml-5 mt-2">
+            <li>Face masks required</li>
+            <li>Hand sanitizer provided</li>
+            <li>Outdoor seating available</li>
+          </ul>
+          </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="reservation-rules">
+        <AccordionTrigger class="flex items-center justify-between">
+          <span class="font-medium">Reservation Rules</span>
+          <!-- <Info class="w-5 h-5 text-gray-500 dark:text-gray-400" /> -->
+        </AccordionTrigger>
+        <AccordionContent>
+          <p class="mt-2">Please arrive 10 minutes before your reservation. No outside food or drinks allowed.</p>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="cancelation-policy">
+        <AccordionTrigger class="flex items-center justify-between">
+          <span class="font-medium">Cancellation Policy</span>
+          <!-- <XCircle class="w-5 h-5 text-gray-500 dark:text-gray-400" /> -->
+        </AccordionTrigger>
+        <AccordionContent>
+          <p class="mt-2">Free cancellation within 24 hours. Late cancellations incur a fee.</p>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  </div>
+
         <div class="px-4 py-4 dark:bg-gray-800">
       <h3 class="text-lg font-semibold mb-4 text-black">Venues you might like</h3>
       <div class="flex space-x-4 overflow-x-auto pb-4">
@@ -182,3 +379,11 @@ const date = ref<Date>()
     </Card>
   </div>
 </template>
+
+<style>
+ .OrangeCol {   
+  background-color: #ff6900;
+ }
+</style>
+
+
