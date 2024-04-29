b902f7b5fc324c18f74732cee73fe11aa0f1f22f
Rashid Refai <rifairashid@gmail.com>
2024-04-28 20:39:04 -0700

    bg test

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -3,11 +3,13 @@ const user = useSupabaseUser()
 </script>
 
 <template>
+  <div class="bg-gray-800">
   <div class="container" style="padding: 50px 0 100px 0">
     <Account v-if="user" />
     <div v-else>
       <h1 class="title">You need to be logged in to view this page</h1>
       <NuxtLink to="/login">Login</NuxtLink>
     </div>
+  </div>
   </div>
 </template>
