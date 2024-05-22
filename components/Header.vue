504bf68d036f37ac8611e527e3801dab85bd8714
Rashid Refai <rifairashid@gmail.com>
2024-05-22 16:12:44 -0700

    text sizes

--- components/Header.vue
+++ components/Header.vue
@@ -13,7 +13,7 @@ import { CrownIcon } from 'lucide-vue-next';
 
 <template>
         <div class=" w-full h-16 flex items-center justify-between px-4 headerMain shadow-md">
-      <a class="flex items-center gap-2 font-semibold text-lg" href="/" rel="ugc">
+      <a class="flex items-center gap-2 font-semibold text-md" href="/" rel="ugc">
         <!-- <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -32,36 +32,36 @@ import { CrownIcon } from 'lucide-vue-next';
         <img src="public/Launch_Gala_Logo-removebgHD.png" class="w-16 h-16">
         <span class=" colorOrange">Launch Gala</span>
       </a>
-      <nav class="hidden lg:flex gap-8 ml-28">
+      <nav class="hidden lg:flex gap-12 ml-28">
         <NuxtLink to="AllVenues">
-          <a class="text-m font-semibold  hover:underline">
+          <a class="text-sm font-semibold  hover:underline">
             Venues
           </a>
         </NuxtLink>
         <NuxtLink to="AllVolunteers">
-          <a class="text-m font-semibold  hover:underline">
+          <a class="text-sm font-semibold  hover:underline">
             Volunteers
           </a>
         </NuxtLink>
         <NuxtLink to="AllSponsors">
-          <a class="text-m font-semibold  hover:underline">
+          <a class="text-sm font-semibold  hover:underline">
             Sponsors
           </a>
         </NuxtLink>
         <NuxtLink to="AllSpeakers">
-          <a class="text-m font-semibold  hover:underline">
+          <a class="text-sm font-semibold  hover:underline">
             Experts
           </a>
         </NuxtLink>
         <NuxtLink to="AllVendors">
 
-<a class="text-m font-semibold  hover:underline">
+<a class="text-sm font-semibold  hover:underline">
   Vendors
 </a>
 
 </NuxtLink>
         <NuxtLink to="AllEvents">
-          <a class="text-m font-semibold  hover:underline">
+          <a class="text-sm font-semibold  hover:underline">
             Events
           </a>
         </NuxtLink>
@@ -103,7 +103,7 @@ import { CrownIcon } from 'lucide-vue-next';
 
         </NuxtLink> -->
         <NuxtLink to="AllCommunities">
-          <a class="text-m font-semibold  hover:underline ">
+          <a class="text-sm font-semibold  hover:underline ">
             Launch AI
           </a>
           <Badge class="  text-xs border-orange-500 bg-white text-orange-500 mr-6">
@@ -112,7 +112,7 @@ import { CrownIcon } from 'lucide-vue-next';
         </NuxtLink>
       </nav>
       <NuxtLink to="CreateEvent">
-      <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-m font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black hover:underline h-10 px-4 py-2 mr-4 ">
+      <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black hover:underline h-10 px-4 py-2 mr-4 ">
         + Create Event
       </button>
       </NuxtLink>
@@ -121,16 +121,16 @@ import { CrownIcon } from 'lucide-vue-next';
   
         <spam class="text-md ml-1">Pro</spam>
         </Badge>
-      <span class="absolute top-5 right-24 -mt-3 mr-6 bg-white text-orange-500 rounded-full text-xs px-1">
+      <span class="absolute top-5 right-24 -mt-3 mr-4 bg-white text-orange-500 rounded-full text-xs px-1">
                         11
                     </span>
     <NuxtLink v-if="!isLoggedIn" to="logIn">
-      <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-m font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-orange-500 text-primary-foreground hover:bg-orange-400 h-10 px-4 py-2 ml-auto">
+      <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-orange-500 text-primary-foreground hover:bg-orange-400 h-10 px-4 py-2 ml-auto">
         Login
       </button>
     </NuxtLink>
       <NuxtLink v-else to="UserDashboard">
-      <button class="inline-flex items-center justify-center border whitespace-nowrap rounded-md text-m font-medium disabled:pointer-events-none  border-orange-500 bg-white text-orange-500 hover:bg-orange-500 hover:text-white h-10 px-4 py-2 ml-auto">
+      <button class="inline-flex items-center justify-center border whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none  border-orange-500 bg-white text-orange-500 hover:bg-orange-500 hover:text-white h-10 px-4 py-2 ml-auto">
         Dashboard 
       </button>
     </NuxtLink>
