a7279c60fddbed2370c0731d70b1bd567843757a
Rashid Refai <rifairashid@gmail.com>
2025-02-07 17:01:04 -0800

    profile

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
+const user = useSupabaseUser()
+</script>
+
+<template>
+  <div >
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="user" />
+    <div v-else>
+      <h1 class="title">You need to be logged in to view this page</h1>
+      <NuxtLink to="/login">Login</NuxtLink>
+    </div>
+  </div>
+  </div>
+</template>
+ -->
+
+ <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { 
   MapPin, Globe, Linkedin, UserPlus, Github, Twitter, 
